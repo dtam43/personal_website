ca60cd60ea878a5d1730d3ccb3e4e8a68d58c3aa
dtam43 <dtam43@uwo.ca>
2023-06-22 19:02:13 -0400

    home page implementation and project previews

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var backToTopLink = document.querySelector(".back-to-top a");
+
+    backToTopLink.addEventListener("click", function(event) {
+      event.preventDefault();
+      var scrollToTop = window.setInterval(function() {
+        var currentPosition = window.pageYOffset;
+        if (currentPosition > 0) {
+          window.scrollTo(0, currentPosition - 50);
+        } else {
+          window.clearInterval(scrollToTop);
+        }
+      }, 16);
+    });
+  });
