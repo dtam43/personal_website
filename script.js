3e26083d7306d5de3064e0e6f9fd1ddc771f8c41
dtam43 <dtam43@uwo.ca>
2023-06-26 18:51:18 -0400

    ruin raider project overview
    still need to add future improvements and a dark mode title

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Code to scroll back to top when clicked
+// To scroll back to top when clicked
 document.addEventListener("DOMContentLoaded", function() {
     var backToTopLink = document.querySelector(".back-to-top a");
 
@@ -12,5 +12,30 @@ document.addEventListener("DOMContentLoaded", function() {
           window.clearInterval(scrollToTop);
         }
       }, 16);
+    });
+  });
+
+  // To check for dark mode on loading different pages
+  document.addEventListener("DOMContentLoaded", function() {
+    const toggle = document.getElementById("toggle");
+    const body = document.body;
+
+    // Check if the toggle state exists in local storage
+    const isDarkMode = localStorage.getItem("darkMode") === "true";
+    toggle.checked = isDarkMode;
+    if (isDarkMode) {
+      body.classList.add("dark-mode");
+    }
+
+    // Add event listener to toggle switch
+    toggle.addEventListener("change", function() {
+      // Add toggle records to local storage
+      if (this.checked) {
+        body.classList.add("dark-mode");
+        localStorage.setItem("darkMode", "true");
+      } else {
+        body.classList.remove("dark-mode");
+        localStorage.setItem("darkMode", "false");
+      }
     });
   });
