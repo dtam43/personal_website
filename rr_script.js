eab3083b903e0f110a2e95a38f477d846dc8eefa
dtam43 <dtam43@uwo.ca>
2023-06-26 18:59:27 -0400

    dark mode logo

---
+++ rr_script.js
@@ -0,0 +1,47 @@
+// To scroll back to top when clicked
+document.addEventListener("DOMContentLoaded", function() {
+    var backToTopLink = document.querySelector(".back-to-top a");
+
+    backToTopLink.addEventListener("click", function(event) {
+      event.preventDefault();
+      var scrollToTop = window.setInterval(function() {
+        var currentPosition = window.pageYOffset;
+        if (currentPosition > 0) {
+          window.scrollTo(0, currentPosition - 50);
+        } else {
+          window.clearInterval(scrollToTop);
+        }
+      }, 16);
+    });
+  });
+
+  // To check for dark mode on loading different pages
+  document.addEventListener("DOMContentLoaded", function() {
+    const toggle = document.getElementById("toggle");
+    const body = document.body;
+    const image = document.querySelector(
+        'img[alt="ruin-raider-title"]'
+      );
+
+    // Check if the toggle state exists in local storage
+    const isDarkMode = localStorage.getItem("darkMode") === "true";
+    toggle.checked = isDarkMode;
+    if (isDarkMode) {
+      body.classList.add("dark-mode");
+      image.src = "/assets/ruin_raider/title_dark.png"
+    }
+
+    // Add event listener to toggle switch
+    toggle.addEventListener("change", function() {
+      // Add toggle records to local storage
+      if (this.checked) {
+        body.classList.add("dark-mode");
+        localStorage.setItem("darkMode", "true");
+        image.src = "/assets/ruin_raider/title_dark.png"
+      } else {
+        body.classList.remove("dark-mode");
+        localStorage.setItem("darkMode", "false");
+        image.src = "/assets/ruin_raider/title.png"
+      }
+    });
+  });
